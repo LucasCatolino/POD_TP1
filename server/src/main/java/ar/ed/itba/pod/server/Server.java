dd18f38f99c824eaaa2b6abbc7849b128e150bf1
Lucas <lcatolino@fi.uba.ar>
2022-09-04 15:47:29 -0300

    Add: archetype structure

---
+++ server/src/main/java/ar/ed/itba/pod/server/Server.java
@@ -0,0 +1,12 @@
+package ar.ed.itba.pod.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("POD_TP1 Server Starting ...");
+    }
+}
